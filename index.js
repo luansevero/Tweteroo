e8bf0054cd3213a9518786856054bae808abece6
Luan Severo <luansevero@icloud.com>
2022-06-17 20:52:13 -0300

    post tweets

--- index.js
+++ index.js
@@ -3,22 +3,33 @@ import cors from 'cors';
 
 const app = express();
 
-const user ={
+app.use(cors());
+app.use(express.json());
+
+const loggedUser ={
     username: "",
     avatar: ""
-}
+};
 
 const tweet ={
     username: "",
     avatar: ""
-}
+};
+
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
-
+    loggedUser.push(req.body);
+    res.send(loggedUser);
 });
 
 app.post("/tweets", (req, res) => {
-
+    tweets.push(req.body);
+    if(tweets.length > 10){
+        const lastTen = tweets.splice((tweets.length - 10), 10);
+        res.send(lastTen)
+    }
+    res.send(tweet)
 });
 
 app.get("/tweets", (req, res) => {
